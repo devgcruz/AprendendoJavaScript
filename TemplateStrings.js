1c9493d20d2ec5112ab7ec54b494aa47688f8d46
Guilherme Cruz <dev.gcruz@gmail.com>
2022-08-09 16:21:19 -0300

    TemplateString

---
+++ TemplateStrings.js
@@ -0,0 +1,17 @@
+const nome = 'Guilherme'
+const concatenacao = 'Dev ' + nome + ' esta em evolucao.'
+
+const template = `
+    Olá ${nome} !`
+
+
+console.log(concatenacao, template)
+
+
+//expressoes
+
+console.log(`1+1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+
+console.log(`Ei... ${up('cuidado')} !`)
