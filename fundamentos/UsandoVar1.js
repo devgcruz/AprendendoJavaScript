696149d83a1b18420d91b78965d6507b5c0e3521
Guilherme Cruz <dev.gcruz@gmail.com>
2022-08-12 18:15:58 -0300

    Entendento var

---
+++ fundamentos/UsandoVar1.js
@@ -0,0 +1,21 @@
+{
+    { 
+        { 
+            { 
+                var teste = 'teste ?'
+            } 
+        } 
+    }
+}
+
+console.log(teste)
+
+function functionTeste() {
+
+    var local = 123
+    console.log(local)
+
+}
+
+functionTeste()
+//console.log(local)
