139956ebe8dfe1b25f752a5511bc7c4d2edc44d8
Guilherme Cruz <dev.gcruz@gmail.com>
2022-08-12 19:19:28 -0300

    Let em Loop 2

---
+++ fundamentos/UsandoLet2.js
@@ -0,0 +1,30 @@
+/* const funcs = []
+
+for (var i = 0; i < 10; i++) {
+
+    funcs.push(function () {
+
+        console.log(i)
+
+    })
+
+}
+
+funcs[8]()
+funcs[2]() */
+
+
+const funcs = []
+
+for (let i = 0; i < 10; i++) {
+
+    funcs.push(function () {
+
+        console.log(i)
+
+    })
+
+}
+
+funcs[8]()
+funcs[2]()
